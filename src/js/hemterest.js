3a6fcc215666387da12947400be2b682b89c82e0
Urban Sandén <hej@urre.me>
2021-01-10 09:05:49 +0100

    Simplify

--- src/js/hemterest.js
+++ src/js/hemterest.js
@@ -1,59 +1,35 @@
 const findImages = () => {
-	const carouselImages = document.querySelector('[data-images]')
+  const carouselImages = document.querySelectorAll(
+    ".gallery-carousel__image-touchable img"
+  );
 
-	const images = JSON.parse(carouselImages.dataset.images)
-	let imageUrls = []
-	for (let image of images) {
-		imageUrls.push(image.url)
-	}
+  let imageUrls = [];
+  for (let image of carouselImages) {
+    imageUrls.push(image.src);
+  }
 
-	return imageUrls
-}
+  return imageUrls;
+};
 
-const addPrice = () => {
-	const lastImage = document.querySelector('.hemterest-wrapper > figure')
-		.parentElement.firstChild
-	const firstImage = document.querySelector('.hemterest-wrapper > figure')
-		.parentElement.firstChild
-	const lastImageClone = lastImage.cloneNode(true)
-	document.querySelector('.hemterest-wrapper').appendChild(lastImageClone)
+setTimeout(() => {
+  createGrid(findImages());
+}, 1500);
 
-	const priceTag = document.createElement('a')
-	priceTag.innerHTML = document.querySelector('.property__price').innerHTML
-	priceTag.classList.add('hemterest-button')
-	priceTag.classList.add('hemterest-price')
-	priceTag.href = '#item-info'
+const createGrid = (urls) => {
 
-	document.querySelector('.hemterest-wrapper').parentElement.prepend(priceTag)
-}
+  const wrapper = document.createElement("div");
+  wrapper.classList.add(`hemterest-wrapper`);
+  wrapper.classList.add(`hemterest-wrapper-${urls.length}`);
+  let figureSize = {};
 
-const cloneRibbon = () => {
-	if (document.querySelector('.ribbon--ongoing-bidding')) {
-		const ribbon = document.querySelector('.ribbon--ongoing-bidding')
-		const ribbonClone = ribbon.cloneNode(true)
-		document.querySelector('.hemterest-price').appendChild(ribbonClone)
-	}
-}
+  for (let url of urls) {
+    const itemFigure = document.createElement("figure");
+    const itemImage = document.createElement("img");
+    itemImage.src = url;
+    itemFigure.appendChild(itemImage);
 
-const createGrid = urls => {
-	const wrapper = document.createElement('div')
-	wrapper.classList.add('hemterest-wrapper')
-	let figureSize = {}
+    wrapper.appendChild(itemFigure);
+  }
 
-	for (let url of urls) {
-		const itemFigure = document.createElement('figure')
-		const itemImage = document.createElement('img')
-		itemImage.src = url
-		itemFigure.appendChild(itemImage)
-
-		wrapper.appendChild(itemFigure)
-	}
-
-	document.querySelector('.page-content').prepend(wrapper)
-}
-
-if (document.querySelector('.property__container')) {
-	createGrid(findImages())
-	cloneRibbon()
-	addPrice()
-}
+  document.querySelector(".listing-container").prepend(wrapper);
+};
